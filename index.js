2a21861042c66a3a488758c720f1dded8385bacd
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-24 22:26:12 +0200

    Turn arrays, iterators and promises into observables using from

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {of} from "rxjs";
+import {from} from "rxjs";
 
 const observer = {
   next: val => console.log('next', val),
@@ -6,6 +6,25 @@ const observer = {
   complete: () => console.log('complete!')
 };
 
-const source$ = of(1, 2, 3, 4, 5);
-
+// arrays => observable
+const source$ = from([1, 2, 3, 4, 5]);
 source$.subscribe(observer);
+const sourceTwo$ = from('Hello');
+sourceTwo$.subscribe(observer);
+
+//promises => observable
+const sourceThree$ = from(fetch('https://api.github.com/users/octocat'));
+sourceThree$.subscribe(observer);
+
+//iterator => observable
+function* hello() {
+  yield 'Hello';
+  yield 'World';
+}
+
+const iterator = hello();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+
+const sourceFour$ = from(iterator);
+sourceFour$.subscribe(observer);
