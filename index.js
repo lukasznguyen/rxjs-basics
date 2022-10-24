77baa0ec6546f80c5ed25c952de85a4aefe6aa45
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-24 22:07:27 +0200

    Create observables from static values using of

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {fromEvent} from "rxjs";
+import {of} from "rxjs";
 
 const observer = {
   next: val => console.log('next', val),
@@ -6,12 +6,6 @@ const observer = {
   complete: () => console.log('complete!')
 };
 
-const source$ = fromEvent(document, 'keyup');
+const source$ = of(1, 2, 3, 4, 5);
 
-const subOne = source$.subscribe(observer);
-const subTwo = source$.subscribe(observer);
-
-setTimeout(() => {
-  console.log('unsubscribing');
-  subOne.unsubscribe();
-}, 3000);
+source$.subscribe(observer);
