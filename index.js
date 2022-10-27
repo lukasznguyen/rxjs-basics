d5853dd6e54a6fc92e47b7973284d3af183ed3fb
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 16:01:36 +0200

    Ignore emissions when an inner observable is active with exhaustMap

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import {fromEvent, interval} from "rxjs";
-import {concatMap, take} from "rxjs/operators";
+import {exhaustMap, take} from "rxjs/operators";
 
 const intervals$ = interval(1000);
 const clicks$ = fromEvent(document, 'click');
 
 clicks$.pipe(
-    concatMap(() => intervals$.pipe(take(3)))
+    exhaustMap(() => intervals$.pipe(take(3)))
 ).subscribe(console.log);
