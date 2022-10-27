1a0e6b0dca3fe7c59e3cc6e24737d186199526a6
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 22:59:38 +0200

    Receive the latest values from multiple observables on emissions using combineLatest

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-import {fromEvent, merge} from "rxjs";
+import {combineLatest, fromEvent, interval} from "rxjs";
+import {filter, map, withLatestFrom} from "rxjs/operators";
 
+//elements
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+//streams
 const keyup$ = fromEvent(document, 'keyup');
 const click$ = fromEvent(document, 'click');
 
-keyup$.subscribe(console.log);
-click$.subscribe(console.log);
+//helpers
+const keyupAsValue = elem => {
+  return fromEvent(elem, 'keyup').pipe(
+      map(event => event.target.valueAsNumber)
+  )
+}
 
-merge(
-    keyup$,
-    click$
+click$.pipe(
+    withLatestFrom(interval(1000))
+).subscribe(console.log);
+
+combineLatest(
+    keyupAsValue(first),
+    keyupAsValue(second)
+).pipe(
+    filter(([first, second]) => {
+      return !isNaN(first) && !isNaN(second);
+    }),
+    map(([first, second]) => first + second)
 ).subscribe(console.log);
