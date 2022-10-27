f076116b4cc4c3be4b9ec1b836519cc1d8e53dd5
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 23:06:04 +0200

    Receive the latest value from multiple observable on completion with forkJoin

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-import {combineLatest, fromEvent, interval} from "rxjs";
-import {filter, map, withLatestFrom} from "rxjs/operators";
+import {forkJoin, of} from 'rxjs';
+import {delay} from "rxjs/operators";
 
-//elements
-const first = document.getElementById('first');
-const second = document.getElementById('second');
 //streams
-const keyup$ = fromEvent(document, 'keyup');
-const click$ = fromEvent(document, 'click');
+const numbers$ = of(1, 2, 3);
+const letters$ = of('a', 'b', 'c');
 
-//helpers
-const keyupAsValue = elem => {
-  return fromEvent(elem, 'keyup').pipe(
-      map(event => event.target.valueAsNumber)
-  )
-}
-
-click$.pipe(
-    withLatestFrom(interval(1000))
-).subscribe(console.log);
-
-combineLatest(
-    keyupAsValue(first),
-    keyupAsValue(second)
-).pipe(
-    filter(([first, second]) => {
-      return !isNaN(first) && !isNaN(second);
-    }),
-    map(([first, second]) => first + second)
-).subscribe(console.log);
+forkJoin({
+  numbers: numbers$,
+  letters: letters$.pipe(delay(3000))
+}).subscribe(console.log);
