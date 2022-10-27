20b3e4cd81fb853ecebd3df8ba2b65e9161ade01
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 22:26:40 +0200

    Append values to a stream using startWith and endWith

--- index.js
+++ index.js
@@ -1,36 +1,8 @@
-import {fromEvent, timer} from "rxjs";
-import {ajax} from "rxjs/ajax";
-import {
-  exhaustMap,
-  finalize,
-  pluck,
-  switchMapTo,
-  takeUntil,
-  tap
-} from "rxjs/operators";
+import {of} from 'rxjs';
+import {startWith, endWith} from "rxjs/operators";
 
-// elements
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const pollingStatus = document.getElementById('polling-status');
-const dogImage = document.getElementById('dog');
-
-//streams
-const startClick$ = fromEvent(startButton, 'click');
-const stopClick$ = fromEvent(stopButton, 'click');
-
-startClick$.pipe(
-    // map to interval
-    exhaustMap(() => timer(0, 5000).pipe(
-        tap(() => pollingStatus.innerHTML = 'Active'),
-        switchMapTo(
-            ajax.getJSON(
-                'https://random.dog/woof.json'
-            ).pipe(
-                pluck('url')
-            )
-        ),
-        takeUntil(stopClick$),
-        finalize(() => pollingStatus.innerHTML = 'Stopped')
-    ))
-).subscribe(url => dogImage.src = url);
+const numbers$ = of(1, 2, 3);
+numbers$.pipe(
+    startWith('a', 'b', 'c'),
+    endWith('x', 'y', 'z')
+).subscribe(console.log);
