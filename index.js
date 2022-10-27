b5ed8276cfe8fbb504e88426420929c8ba45ad0e
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 13:20:37 +0200

    Sample a stream on a uniform duration using sampleTime

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import {fromEvent} from "rxjs";
-import {debounceTime, throttleTime} from "rxjs/operators";
+import {fromEvent, interval} from "rxjs";
+import {sampleTime, map, sample} from "rxjs/operators";
 
 //streams
 const click$ = fromEvent(document, 'click');
 
 click$.pipe(
-    throttleTime(3000)
+    sampleTime(4000),
+    map(({clientX, clientY}) => ({
+      clientX, clientY
+    }))
+).subscribe(console.log);
+
+const timer$ = interval(1000);
+timer$.pipe(
+    sample(click$)
 ).subscribe(console.log);
