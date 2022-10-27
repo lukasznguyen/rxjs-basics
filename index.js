36f6e1d5e17bda2391001ebd3de5262ff3440f2e
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 13:24:51 +0200

    Audit a stream for a duration after an event occurs using auditTime

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-import {fromEvent, interval} from "rxjs";
-import {sampleTime, map, sample} from "rxjs/operators";
+import {fromEvent} from "rxjs";
+import {auditTime} from "rxjs/operators";
 
 //streams
 const click$ = fromEvent(document, 'click');
 
 click$.pipe(
-    sampleTime(4000),
-    map(({clientX, clientY}) => ({
-      clientX, clientY
-    }))
-).subscribe(console.log);
-
-const timer$ = interval(1000);
-timer$.pipe(
-    sample(click$)
+    auditTime(4000)
 ).subscribe(console.log);
