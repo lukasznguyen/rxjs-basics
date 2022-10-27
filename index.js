5784b4689de277246650a97e4b5ac077dfdb8c9e
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 13:12:37 +0200

    Ignore values during windows using throttleTime

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-import {fromEvent, interval} from "rxjs";
-import {debounceTime, pluck, distinctUntilChanged, debounce} from "rxjs/operators";
-
+import {fromEvent} from "rxjs";
+import {debounceTime, throttleTime} from "rxjs/operators";
 
 //streams
 const click$ = fromEvent(document, 'click');
 
 click$.pipe(
-    debounceTime(1000)
-).subscribe(console.log);
-
-//elements
-const inputBox = document.getElementById('text-input');
-const input$ = fromEvent(inputBox, 'keyup');
-input$.pipe(
-    debounceTime(1000),
-    pluck('target', 'value'),
-    distinctUntilChanged()
-).subscribe(console.log);
-
-input$.pipe(
-    debounce(() => interval(1000)),
-    pluck('target', 'value'),
-    distinctUntilChanged()
+    throttleTime(3000)
 ).subscribe(console.log);
