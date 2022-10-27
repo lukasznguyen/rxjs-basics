81c2156eee36026034d92581c0302d87d085babb
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-27 22:35:22 +0200

    Queue observable execution using concat

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import {of} from 'rxjs';
-import {startWith, endWith} from "rxjs/operators";
+import {empty, interval} from 'rxjs';
+import {concat, delay, startWith} from "rxjs/operators";
 
-const numbers$ = of(1, 2, 3);
-numbers$.pipe(
-    startWith('a', 'b', 'c'),
-    endWith('x', 'y', 'z')
+const interval$ = interval(1000);
+const delayed$ = empty().pipe(delay(1000));
+
+delayed$.pipe(
+    concat(
+        delayed$.pipe(startWith('3....')),
+        delayed$.pipe(startWith('2....')),
+        delayed$.pipe(startWith('1....')),
+        delayed$.pipe(startWith('Go!'))
+    ),
+    startWith('Get ready!')
 ).subscribe(console.log);
