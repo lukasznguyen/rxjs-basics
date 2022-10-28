24045df4f67a0e748ca553d28ab3d46075066fad
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-28 11:12:06 +0200

    Lab 4. Create a Mortgage Calculator

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-import {forkJoin, of} from 'rxjs';
-import {delay} from "rxjs/operators";
+import {calculateMortgage} from "./calculate-mortgage";
+import {combineLatest, fromEvent, of} from "rxjs";
+import {delay, filter, map, mergeMap, share, tap} from "rxjs/operators"
 
-//streams
-const numbers$ = of(1, 2, 3);
-const letters$ = of('a', 'b', 'c');
+const loanAmount = document.getElementById('loanAmount');
+const interest = document.getElementById('interest');
+const loanLength = document.querySelectorAll('.loanLength');
+const expected = document.getElementById('expected');
 
-forkJoin({
-  numbers: numbers$,
-  letters: letters$.pipe(delay(3000))
-}).subscribe(console.log);
+//helpers
+const createInputValueStream = elem => {
+  return fromEvent(elem, 'input').pipe(
+      map(event => parseFloat(event.target.value))
+  )
+}
+
+const saveResponse = mortgageAmount => {
+  return of(mortgageAmount).pipe(
+      delay(1000)
+  )
+}
+
+// streams
+const interest$ = createInputValueStream(interest);
+const loanLength$ = createInputValueStream(loanLength);
+const loanAmount$ = createInputValueStream(loanAmount);
+
+const calculation$ = combineLatest(
+    interest$,
+    loanAmount$,
+    loanLength$
+).pipe(
+    map(([interest, loanAmount, loanLength]) => {
+      return calculateMortgage(interest, loanAmount, loanLength);
+    }),
+    tap(console.log),
+    filter(mortgageAmount => !isNaN(mortgageAmount)),
+    share()
+);
+
+calculation$.subscribe(mortgageAmount => {
+  expected.innerHTML = mortgageAmount;
+})
+
+calculation$.pipe(
+    mergeMap(mortgageAmount => saveResponse(mortgageAmount))
+).subscribe()
